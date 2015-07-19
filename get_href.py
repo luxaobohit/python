d07b963b887a29aecda017cc1d8446d6fc8a800f
luxaobohit <luxaobohit@users.noreply.github.com>
2015-07-19 10:46:32 +0800

    Update get_href.py

--- get_href.py
+++ get_href.py
@@ -12,13 +12,10 @@ hrefs = soup.select('a[href]') #get the href list
 pat = re.compile(r'href="([^"]*)"')
 pat2 = re.compile(r'http')
 for item in hrefs:
+    h = pat.search(str(item))
+    href = h.group(1)
+    if pat2.search(str(item)):
+        link = href
+    else:
+        link = url + href
 
-pat = re.compile(r'href="([^"]*)"')
-11     pat2 = re.compile(r'http')
-12     for item in content:
-13    h = pat.search(str(item))
-14         href = h.group(1)
-15         if pat2.search(href):
-16             ans = href
-17         else:
-18             ans = url+href
