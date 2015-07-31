2c98ffbe82fceb365d934cc1ebf7fe15b26ab6a2
luxaobohit <luxaobohit@users.noreply.github.com>
2015-07-31 13:10:42 +0800

    Update get_href.py

--- get_href.py
+++ get_href.py
@@ -10,6 +10,7 @@ res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html5lib')
 hrefs = soup.select('a[href]') #get the href list
 pat = re.compile(r'href="([^"]*)"')
+#上面正则表达式中的括号“()”叫做捕获型括号，可以获得括号里实际匹配的文本
 pat2 = re.compile(r'http')
 for item in hrefs:
     h = pat.search(str(item))
